20863f9b5fc7b3857618294df3033bbf96e39185
Shivam <shivamjaiswal648@gmail.com>
2023-06-30 22:29:52 +0530

    If else stataement for single line

--- functions.js
+++ functions.js
@@ -1,5 +1,15 @@
 "use strict";
-function add(num1, num2) {
-    return num1 + num2;
+function add(num1, num2, num3) {
+    return num3 ? num1 + num2 + num3 : num1 + num2;
 }
-console.log(add(10, 20));
+console.log(add(10, 20, 30));
+// function add1(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// console.log(add1(10, 20));
+// const sub = (num1: number, num2: number): number => num1 - num2;
+// console.log(sub(10, 20));
+// const mult = function (num1: number, num2: number): number {
+//   return num1 * num2;
+// };
+// console.log(mult(10, 20));
--- functions.ts
+++ functions.ts
@@ -1,13 +1,17 @@
-function add(num1: number, num2: number) {
-  return num1 + num2;
+function add(num1: number, num2: number, num3?: number): number {
+  return num3 ? num1 + num2 + num3 : num1 + num2;
 }
+console.log(add(10, 20, 30));
 
-console.log(add(10, 20));
+// function add1(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// console.log(add1(10, 20));
 
-const sub = (num1: number, num2: number): number => num1 - num2;
-console.log(sub(10, 20));
+// const sub = (num1: number, num2: number): number => num1 - num2;
+// console.log(sub(10, 20));
 
-const mult = function (num1: number, num2: number): number {
-  return num1 * num2;
-};
-console.log(mult(10, 20));
+// const mult = function (num1: number, num2: number): number {
+//   return num1 * num2;
+// };
+// console.log(mult(10, 20));
