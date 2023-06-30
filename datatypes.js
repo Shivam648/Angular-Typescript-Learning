63a72924b24523b613d689bed0346995c1de07f9
Shivam <shivamjaiswal648@gmail.com>
2023-06-30 21:54:33 +0530

    Add .gitattributes file to control line endings

--- datatypes.js
+++ datatypes.js
@@ -1,10 +1,7 @@
+"use strict";
 // Checking Type safety in TS and Compare it with JS
-
-let Lname : string;
+let Lname;
 Lname = 'Shivam';
 let newname = Lname.toUpperCase();
-
 console.log(newname);
-
-
 // Lname = 10;
---
+++ test.js
@@ -0,0 +1,5 @@
+// Checking Type saftey in TS and Compare it with JS
+
+let Lname = "Shivam"
+
+Lname = 10;
